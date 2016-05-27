afb6b088505e5fbaeda214113a5b64b52c85c8c2
Dirk Weissenborn <dirk.weissenborn@gmail.com>
2016-05-27 14:14:48 +0200

    split supporting evidence and  query

--- deepmind_rc/sampler.py
+++ deepmind_rc/sampler.py
@@ -34,20 +34,28 @@ class BatchSampler:
 
     def get_batch(self):
         '''
+        Note, the data in the kb is structured as follows: context= supporting_evidence + "||" + query.
+        Thus, we have to split contexts.
         :return: list of (context, query_positions, negative_candidates, supporting_facts)
         '''
         if self.end_of_epoch():
             print("WARNING: End of epoch reached in sampler. Resetting automatically.")
             self.reset()
         batch = ([], [], [], [])
+        splitter = self.kb.id("||")
         for i in range(self.batch_size):
             context = self.kb.context(self.which_set, self.todo[i])
+            k = len(context)-1
+            while context[k] != splitter:
+                k -= 1
+            # < k: supporting evidence; >k: query
             positions = self.kb.positions(self.which_set, self.todo[i])
-            neg_cands = [context[x] for x in positions if context[x] != context[positions[-1]]]
-            batch[0].append(context)
-            batch[1].append([positions[-1]])
-            batch[2].append([neg_cands])
-            batch[3].append([(None, positions[:-1])])
+            batch[0].append(context[k+1:])  # query
+            batch[1].append([positions[-1]-k-1])  # position to be predicted in query
+
+            neg_cands = [context[x] for x in positions if x < k and context[x] != context[positions[-1]]]
+            batch[2].append([neg_cands])  # negative candidates are all entities within supporting evidence that are not the answer
+            batch[3].append([(context[:k], [p for p in positions if p < k])])  # points of interest in supporting evidence
 
         return batch
 
